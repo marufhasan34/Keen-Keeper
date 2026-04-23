2a5c8452f7b9b28bcd52781eebda30c458d9e0a9
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-04-23 19:34:34 +0600

    fix navbar responsive issue

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,15 +8,15 @@ import MyNavLink from "./MyNavLink";
 const Navbar = () => {
   return (
     <div className="bg-base-100 mt-3  shadow-sm">
-      <div className="navbar container mx-auto">
-        <div className="flex-1">
+      <div className="navbar container flex-col md:flex-row items-center  mx-auto">
+        <div className="flex-1 mb-5 md:mb-0">
           <a className="text-green-800 font-semibold text-xl">
             <span className="font-bold text-black">Keen</span>Kepper
           </a>
         </div>
         <div className="flex-none">
           <div className="flex gap-4">
-            <ul className="menu  menu-horizontal px-1">
+            <ul className="menu flex-col md:flex-row menu-horizontal px-1">
               <li className="md:mr-3 text-lg text-gray-500 font-semibold">
                 <MyNavLink to={"/"}>
                   <RiHome2Line /> Home
